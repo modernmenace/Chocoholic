55e1645ef8c46fef7bb7a0bcd83d5ffb729391a2
Jared <jaredbortz@gmail.com>
2020-04-13 15:41:34 -0400

    SQLite

--- Chocoholic/Program.cs
+++ Chocoholic/Program.cs
@@ -8,6 +8,17 @@ namespace Chocoholic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string cs = "Data Source=:memory:";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+            using var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            Console.WriteLine($"SQLite version: {version}");
         }
     }
 }
