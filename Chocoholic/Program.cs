a7235bca2e0b7b84af9250382d453713321d0017
Jared <jaredbortz@gmail.com>
2020-04-13 15:24:15 -0400

    Add project files.

---
+++ Chocoholic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chocoholic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
