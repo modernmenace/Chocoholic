d6372ce72e63c20c0055cbbb5c8bda4bbe325a79
modernmenace <lukebortz1>
2020-05-01 13:06:25 -0400

    Database work

--- Chocoholic/Database.cs
+++ Chocoholic/Database.cs
@@ -29,7 +29,54 @@ public class Database
         }
     }
 
-    //cretae new operator or manager
+    //create new member or provider
+    public void createActor(ActorType type, string name, string address, string city, string state, int zipcode, int status = 1)
+    {
+        //This method can only be used with member and provider types
+        Debug.Assert(type == ActorType.Member || type == ActorType.Provider);
+
+        string cs = @"URI=" + db_file;
+        using var con = new SQLiteConnection(cs);
+        con.Open();
+        using var cmd = new SQLiteCommand(con);
+
+        if (type == ActorType.Member)
+        {
+            cmd.CommandText = @"INSERT INTO Members(name, status, address, city, state, zipcode) VALUES (" + name + ", " + status + ", "
+                + address + ", " + city + ", " + state + ", " + zipcode;
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"SELECT id FROM Members ORDER BY id DESC LIMIT 1;";
+            cmd.ExecuteNonQuery();
+
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine("Member created with id '" + $"{rdr.GetInt32(0)}" + "'!");
+            }
+        }
+        else if (type == ActorType.Provider)
+        {
+            cmd.CommandText = @"INSERT INTO Providers(name, address, city, state, zipcode) VALUES (" + name + ", "
+                + address + ", " + city + ", " + state + ", " + zipcode;
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"SELECT id FROM Providers ORDER BY id DESC LIMIT 1;";
+            cmd.ExecuteNonQuery();
+
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine("Provider created with id '" + $"{rdr.GetInt32(0)}" + "'!");
+            }
+        }
+
+        con.Close();
+    }
+
+    //create new operator or manager
     public void createActor(ActorType type)
     {
         //This method can only be used with operator and manager types
