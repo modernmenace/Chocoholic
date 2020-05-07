2ce10b2cf6ac1c3adc1fa9bb1943f5094b206402
Tier <nmainzinger@gmail.com>
2020-05-07 14:58:07 -0400

    Add files via upload

---
+++ Chocoholic/Manager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+public class Manager
+{
+    private uint oID;
+    private Database database;
+
+    public Manager(uint id, Database db)
+    {
+        ID = id;
+        database = db;
+    }
+
+    public void terminal()
+    {
+        while (true)
+        {
+            string command = Console.ReadLine();
+            var commandArray = command.Split(' ');
+
+            if (commandArray[0].ToLower().Contains("exit"))
+            {
+                System.Environment.Exit(0);
+            }
+            else if(commandArray[0].ToLower() == "search")
+            {
+                Console.WriteLine("Please enter the ID of the provider:");
+                uint mId = 0;
+                try
+                {
+                    mId = UInt32.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ID is not of valid format");
+                    continue;
+                }
+                database.getServiceReport(mId);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Syntax\n");
+                continue;
+            }
+        }
+    }
+
+    public uint ID
+    {
+        get { return oID; }
+        set { oID = value; }
+    }
+}
