423bce13f42927272d08615236db79c0217da95a
modernmenace <lukebortz1>
2020-05-07 19:39:22 -0400

    Manager Implementation

--- Chocoholic/Operator.cs
+++ Chocoholic/Operator.cs
@@ -24,7 +24,7 @@ public class Operator
             {
                 if (commandArray.Length < 2)
                 {
-                    Console.WriteLine("Invalid Syntax\nProper Usage: create (member/provider/service)");
+                    Console.WriteLine("Invalid Syntax\nProper Usage: create (member/provider/service/manager)");
                     continue;
                 }
                 if (commandArray[1].ToLower() == "member")
@@ -91,10 +91,11 @@ public class Operator
                         continue;
                     }
                 }
-
+                else if (commandArray[1].ToLower() == "manager")
+                    database.createActor(Database.ActorType.Manager);
                 else
                 {
-                    Console.WriteLine("Invalid Syntax\nProper Usage: create (member/provider/service)");
+                    Console.WriteLine("Invalid Syntax\nProper Usage: create (member/provider/service/manager)");
                     continue;
                 }
             }
@@ -104,7 +105,7 @@ public class Operator
             {
                 if (commandArray.Length < 2)
                 {
-                    Console.WriteLine("Invalid Syntax\nProper Usage: " + commandArray[0] + " (member/provider/service)");
+                    Console.WriteLine("Invalid Syntax\nProper Usage: " + commandArray[0] + " (member/provider/service/manager)");
                     continue;
                 }
 
@@ -169,9 +170,24 @@ public class Operator
 
                     database.deleteService(mId);
                 }
+                else if (commandArray[1].ToLower() == "manager")
+                {
+                    Console.WriteLine("Please enter the manager id:");
+                    uint mId = 0;
+                    try
+                    {
+                        mId = UInt32.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("ID number is not of valid format");
+                        continue;
+                    }
+                    database.deleteActor(Database.ActorType.Manager, mId);
+                }
                 else
                 {
-                    Console.WriteLine("Invalid Syntax\nProper Usage: " + commandArray[0] + " (member/provider/service)");
+                    Console.WriteLine("Invalid Syntax\nProper Usage: " + commandArray[0] + " (member/provider/service/manager)");
                     continue;
                 }
             }
